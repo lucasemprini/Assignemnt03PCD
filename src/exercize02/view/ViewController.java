7dd416c81cd7c89301fc7ca05bae6b91feae7e2b
lucasemprini <luca.semprini10@studio.unibo.it>
2018-06-01 16:31:26 +0200

    Creata View del secondo esercizio

---
+++ src/exercize02/view/ViewController.java
@@ -0,0 +1,17 @@
+package exercize02.view;
+
+import javafx.scene.control.*;
+
+public class ViewController {
+    public ListView listOfMessages;
+    public ListView actorsList;
+    public TextArea textArea;
+    public Label labelActorInfo;
+    public Button sendButton;
+    public Button addButton;
+    public Button removeButton;
+
+    public void initialize() {}
+
+
+}
