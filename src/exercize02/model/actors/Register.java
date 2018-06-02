2291d4777530a4d2a9f1f2bf38105e0f9d42829f
lucasemprini <luca.semprini10@studio.unibo.it>
2018-06-02 18:55:42 +0200

    Creati file di model dummy per esercizio 2

---
+++ src/exercize02/model/actors/Register.java
@@ -0,0 +1,10 @@
+package exercize02.model.actors;
+
+import akka.actor.AbstractActor;
+
+public class Register extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
+}
---
+++ src/exercize02/model/actors/User.java
@@ -0,0 +1,10 @@
+package exercize02.model.actors;
+
+import akka.actor.AbstractActor;
+
+public class User extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
+}
