16ae580600814b941cda066eb31f99426409a27f
lucasemprini <luca.semprini10@studio.unibo.it>
2018-06-05 15:07:34 +0200

    Aggiunta azione di remove, da migliorare

--- src/exercize02/model/messages/RemActorButtonPressedMsg.java
+++ src/exercize02/model/messages/RemActorButtonPressedMsg.java
@@ -1,4 +1,18 @@
 package exercize02.model.messages;
 
+import akka.actor.ActorRef;
+import javafx.collections.ObservableList;
+
 public class RemActorButtonPressedMsg {
+
+    private final ActorRef removeWho;
+
+    public RemActorButtonPressedMsg(final ActorRef whoSends) {
+        this.removeWho = whoSends;
+    }
+
+    public ActorRef getToBeRemoved() {
+        return this.removeWho;
+    }
+
 }
