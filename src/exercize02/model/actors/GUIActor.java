b709f8a7d4f879755d4b71f104a263b82d75de4e
jacopo <jacopo.riciputi@gmail.com>
2018-06-06 17:15:48 +0200

    Correzione bug con stop dell'attore alla rimozione

--- src/exercize02/model/actors/GUIActor.java
+++ src/exercize02/model/actors/GUIActor.java
@@ -79,7 +79,7 @@ public class GUIActor extends AbstractActor {
                     this.currentChat.clear();
                     System.out.println("Removing chat: " + this.currentChat);
                     this.mapOfChats.remove(toBeRemoved);
-
+                    ActorSystem.create("MySystem").stop(toBeRemoved);
             });
         }).match(GUIShowMsg.class, msg -> {
             /*
